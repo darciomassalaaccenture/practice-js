e4031fa9db69337df1672e472af9987a58b955fa
Darcio Massala <Darciomassala@accenture.com>
2023-07-07 14:59:38 +0100

    pushing

---
+++ index.js
@@ -0,0 +1,60 @@
+// Types
+
+// Learning Objectives
+
+// List the basic types available in JS
+// Recognise and write syntax for strings, numbers and booleans
+// Initialise variables and constants using console.log
+
+// Strings = text written in "", '' or ``
+
+let a = "apple";
+let b = "banana";
+let c = `orange`;
+
+console.log(typeof a);
+
+console.log(typeof b);
+
+console.log(typeof c);
+
+//Numbers = any numerical values
+
+let d = 10;
+let e = 10.5;
+
+console.log(typeof d);
+
+console.log(typeof e);
+
+// How would you use this code?
+
+let name = "Darcio";
+let activity = "write";
+let age = "24";
+
+console.log(
+  "My name is " +
+    name +
+    " and I like to " +
+    activity +
+    ". I am " +
+    age +
+    " years old."
+);
+
+// Operators ( +, -, /, *, %)
+
+let f = 20 % 3;
+
+// modulo works out the remainder of a division
+
+// Expressions often combine numbers and strings
+
+let g = 10 + 5;
+
+console.log(f);
+
+// Questions:
+
+// What can you make with this?
